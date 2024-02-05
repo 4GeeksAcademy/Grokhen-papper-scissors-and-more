2d26520fb439c8ed433902914c0115330fdaf8c4
Grokhen <152092079+Grokhen@users.noreply.github.com>
2024-02-05 20:11:04 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function() {};
+
+let playGame = () => {
+  let gameStart = prompt(
+    "Esto empieza, elige: Piedra, papel, tijeras, lagarto, Spock"
+  ).toLowerCase();
+  const options = ["piedra", "papel", "tijeras", "lagarto", "Spock"];
+
+  let gamePick = options[Math.floor(Math.random() * options.length)];
+
+  if (
+    gameStart === "tijeras" &&
+    (gamePick === "papel" || gamePick === "lagarto")
+  ) {
+    console.log("Has ganado.");
+  } else if (
+    gameStart === "papel" &&
+    (gamePick === "piedra" || gamePick === "spock")
+  ) {
+    console.log("Has ganado.");
+  } else if (
+    gameStart === "piedra" &&
+    (gamePick === "lagarto" || gamePick === "tijeras")
+  ) {
+    console.log("Has ganado.");
+  } else if (
+    gameStart === "lagarto" &&
+    (gamePick === "spock" || gamePick === "papel")
+  ) {
+    console.log("Has ganado.");
+  } else if (
+    gameStart === "spock" &&
+    (gamePick === "tijeras" || gamePick === "piedra")
+  ) {
+    console.log("Has ganado.");
+  } else if (gameStart === gamePick) {
+    console.log("Empate");
+  } else console.log("Has perdido");
 };
+
+playGame();
