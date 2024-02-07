c08f9d08ea0632aa3ad2a742a88ade2f8c0ec0e9
Grokhen <152092079+Grokhen@users.noreply.github.com>
2024-02-07 19:47:38 +0000

    code cleaning

--- src/app.js
+++ src/app.js
@@ -11,38 +11,26 @@ let playGame = () => {
   let playerPick = prompt(
     "Esto empieza, elige: Piedra, papel, tijeras, lagarto, Spock"
   ).toLowerCase();
-  const options = ["piedra", "papel", "tijeras", "lagarto", "Spock"];
+  const options = ["piedra", "papel", "tijeras", "lagarto", "spock"];
 
   let gamePick = options[Math.floor(Math.random() * options.length)];
   console.log(`Tijeratron ha elegido: ${gamePick}`);
 
-  if (
-    playerPick === "tijeras" &&
-    (gamePick === "papel" || gamePick === "lagarto")
-  ) {
-    console.log("Has ganado.");
-  } else if (
-    playerPick === "papel" &&
-    (gamePick === "piedra" || gamePick === "spock")
-  ) {
-    console.log("Has ganado.");
-  } else if (
-    playerPick === "piedra" &&
-    (gamePick === "lagarto" || gamePick === "tijeras")
-  ) {
-    console.log("Has ganado.");
-  } else if (
-    playerPick === "lagarto" &&
-    (gamePick === "spock" || gamePick === "papel")
-  ) {
-    console.log("Has ganado.");
-  } else if (
-    playerPick === "spock" &&
-    (gamePick === "tijeras" || gamePick === "piedra")
-  ) {
-    console.log("Has ganado.");
-  } else if (playerPick === gamePick) {
+  if (playerPick === gamePick) {
     console.log("Empate");
+  } else if (
+    (playerPick === "tijeras" &&
+      (gamePick === "papel" || gamePick === "lagarto")) ||
+    (playerPick === "papel" &&
+      (gamePick === "piedra" || gamePick === "spock")) ||
+    (playerPick === "piedra" &&
+      (gamePick === "lagarto" || gamePick === "tijeras")) ||
+    (playerPick === "lagarto" &&
+      (gamePick === "spock" || gamePick === "papel")) ||
+    (playerPick === "spock" &&
+      (gamePick === "tijeras" || gamePick === "piedra"))
+  ) {
+    console.log("Has ganado.");
   } else console.log("Has perdido");
 };
 
